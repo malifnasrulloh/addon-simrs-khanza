de547b2bcb695d5ab35c9b874e26e17a90a5b215
malifnasrulloh <alifnasrulloh.jbg@gmail.com>
2026-06-07 22:31:13 +0700

    feat(satusehat): add new EpisodeOfCare types mapping in PHP service

--- php-service/lib/satusehat/EpisodeOfCareType.php
+++ php-service/lib/satusehat/EpisodeOfCareType.php
@@ -42,6 +42,72 @@ class EpisodeOfCareType
                 'Tuberkulosis Sensitif Obat',
                 ['A15', 'A16', 'A17', 'A18', 'A19'] // Contains these prefixes
             ),
+            'TB-RO' => new self(
+                'http://terminology.kemkes.go.id/CodeSystem/episodeofcare-type',
+                'TB-RO',
+                'Tuberkulosis Resisten Obat',
+                [] // Handled manually / future use
+            ),
+            'Neonate' => new self(
+                'http://terminology.kemkes.go.id/CodeSystem/episodeofcare-type',
+                'Neonate',
+                'Neonate',
+                ['P', 'Z38']
+            ),
+            'CKD' => new self(
+                'http://terminology.kemkes.go.id/CodeSystem/episodeofcare-type',
+                'CKD',
+                'Chronic Kidney Disease',
+                ['N18']
+            ),
+            'CNC' => new self(
+                'http://terminology.kemkes.go.id/CodeSystem/episodeofcare-type',
+                'CNC',
+                'Cancer Management Care',
+                ['C', 'D0', 'D1', 'D2', 'D3', 'D4', 'Z51.1', 'Z51.0']
+            ),
+            'CAD' => new self(
+                'http://terminology.kemkes.go.id/CodeSystem/episodeofcare-type',
+                'CAD',
+                'Coronary Arterial Disease',
+                ['I20', 'I21', 'I22', 'I23', 'I24', 'I25']
+            ),
+            'CVD' => new self(
+                'http://terminology.kemkes.go.id/CodeSystem/episodeofcare-type',
+                'CVD',
+                'Cerebrovascular Disease',
+                ['I60', 'I61', 'I62', 'I63', 'I64', 'I65', 'I66', 'I67', 'I68', 'I69']
+            ),
+            'hacc' => new self(
+                'http://terminology.hl7.org/CodeSystem/episodeofcare-type',
+                'hacc',
+                'Home and Community Care',
+                []
+            ),
+            'pac' => new self(
+                'http://terminology.hl7.org/CodeSystem/episodeofcare-type',
+                'pac',
+                'Post Acute Care',
+                []
+            ),
+            'diab' => new self(
+                'http://terminology.hl7.org/CodeSystem/episodeofcare-type',
+                'diab',
+                'Post coordinated diabetes program',
+                ['E10', 'E11', 'E12', 'E13', 'E14']
+            ),
+            'da' => new self(
+                'http://terminology.hl7.org/CodeSystem/episodeofcare-type',
+                'da',
+                'Drug and alcohol rehabilitation',
+                ['F10', 'F11', 'F12', 'F13', 'F14', 'F15', 'F16', 'F17', 'F18', 'F19', 'Z71.4', 'Z71.5']
+            ),
+            'cacp' => new self(
+                'http://terminology.hl7.org/CodeSystem/episodeofcare-type',
+                'cacp',
+                'Community-based aged care',
+                []
+            ),
         ];
     }
 
