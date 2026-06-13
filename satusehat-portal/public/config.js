879fc7c8372520d71d3d95f5652d7dbf12e3bc27
malifnasrulloh <alifnasrulloh.jbg@gmail.com>
2026-06-13 20:14:28 +0700

    build: remove React-based satusehat-portal Node.js project

--- satusehat-portal/public/config.js
+++
@@ -1,7 +0,0 @@
-window.PORTAL_CONFIG = {
-    // URL to the PHP backend API (can be relative to work on any domain/port out-of-the-box!)
-    API_URL: "../php-service/api_satusehat_portal.php",
-    
-    // The subdirectory path where this frontend is hosted on your webserver
-    ROUTER_BASENAME: "/ssportal"
-};
