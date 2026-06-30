7726abd12bde4ed18a9883787d3b6760080752cd
malifnasrulloh <alifnasrulloh.jbg@gmail.com>
2026-06-30 17:46:59 +0700

    fix(satusehat): align observation ttv query with simrs app

--- php-service/lib/satusehat/ObservationTTVProcessor.php
+++ php-service/lib/satusehat/ObservationTTVProcessor.php
@@ -114,7 +114,7 @@ class SatuSehatObservationTTVProcessor
                     $noRawat, 
                     $tglObs, 
                     $jamObs, 
-                    $p['status_lanjut'], 
+                    $p['status'], 
                     $idObservation
                 );
 
@@ -137,7 +137,7 @@ class SatuSehatObservationTTVProcessor
                             $noRawat, 
                             $tglObs, 
                             $jamObs, 
-                            $p['status_lanjut'], 
+                            $p['status'], 
                             $idObservation
                         );
                         $this->db->updateObservationLocalState($ttvTypeKey, $noRawat, $tglObs, $jamObs, 'sent');
